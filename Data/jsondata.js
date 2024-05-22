725f2b9401ebe461de5e6e30452a5b2b2fb2210a
Hijas k <hijaskallipparamban@gmail.com>
2024-05-22 20:48:35 +0530

    node project

---
+++ Data/jsondata.js
@@ -0,0 +1,96 @@
+[
+
+    {
+        "id": 1,
+        "name": "APPLE iPhone XR",
+        "color": "White",
+        "ROM": 64,
+        "price": 790,
+        "modelName": "iPhone XR",
+        "modelNumber": "MH6N3HN/A",
+        "size": "15.49 cm (6.1 inch) Display",
+        "camera": "12MP Rear Camera, 7MP Front Camera",
+        "description": "1400:1 Contrast Ratio (Typical), True Tone Display (Six-channel Light Sensor), Wide Colour Display, 625 nits Max Brightness (Typical), Fingerprint-resistant Oleophobic Coating, Support for Display of Multiple Languages and Characters, Retina HD Display, Tap to wake, Wide Colour Gamut",
+        "productImage": "https://cl.alamy.com/zooms/6/e98284ded5444c88949d7fd9f2bee356/2cd68c5.jpg"
+    },
+    {
+        "id": 2,
+        "name": "APPLE iPhone 12",
+        "color": "Blue",
+        "ROM": 128,
+        "price": 1200,
+        "modelName": "iPhone 12",
+        "modelNumber": "MGJ73HN/A",
+        "size": "15.49 cm (6.1 inch) Super Retina XDR Display",
+        "camera": "12MP Dual Rear Camera, 12MP Front Camera",
+        "description": "Super Retina XDR display, Haptic Touch, 1200 nits Max Brightness (HDR), Fingerprint-resistant Oleophobic Coating, Support for Display of Multiple Languages and Characters",
+        "productImage": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-blue-select-2020"
+    },
+    {
+        "id": 3,
+        "name": "APPLE iPhone 12 Pro",
+        "color": "Graphite",
+        "ROM": 256,
+        "price": 1500,
+        "modelName": "iPhone 12 Pro",
+        "modelNumber": "MGLK3HN/A",
+        "size": "15.49 cm (6.1 inch) Super Retina XDR Display",
+        "camera": "12MP Triple Rear Camera, 12MP Front Camera",
+        "description": "Pro camera system with LiDAR scanner, Super Retina XDR display, Haptic Touch, 1200 nits Max Brightness (HDR), Fingerprint-resistant Oleophobic Coating, Support for Display of Multiple Languages and Characters",
+        "productImage": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-graphite-select-2020"
+    },
+    {
+        "id": 4,
+        "name": "APPLE iPhone 13",
+        "color": "Red",
+        "ROM": 128,
+        "price": 1300,
+        "modelName": "iPhone 13",
+        "modelNumber": "MLPG3HN/A",
+        "size": "15.49 cm (6.1 inch) Super Retina XDR Display",
+        "camera": "12MP Dual Rear Camera, 12MP Front Camera",
+        "description": "Super Retina XDR display, Haptic Touch, 800 nits Max Brightness (Typical), Fingerprint-resistant Oleophobic Coating, Support for Display of Multiple Languages and Characters",
+        "productImage": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-red-select-2021"
+    },
+    {
+        "id": 5,
+        "name": "APPLE iPhone 13 Pro",
+        "color": "Gold",
+        "ROM": 256,
+        "price": 1700,
+        "modelName": "iPhone 13 Pro",
+        "modelNumber": "MLVD3HN/A",
+        "size": "15.49 cm (6.1 inch) Super Retina XDR Display",
+        "camera": "12MP Triple Rear Camera, 12MP Front Camera",
+        "description": "Pro camera system with Ultra Wide, Wide, and Telephoto cameras, Super Retina XDR display, Haptic Touch, 1000 nits Max Brightness (Typical), Fingerprint-resistant Oleophobic Coating, Support for Display of Multiple Languages and Characters",
+        "productImage": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pro-gold-select-2021"
+    }, 
+    {
+        "id": 6,
+        "name": "APPLE iPhone SE",
+        "color": "Black",
+        "ROM": 128,
+        "price": 990,
+        "modelName": "iPhone SE",
+        "modelNumber": "WHGT3HN/A",
+        "size": "11.94 cm (4.7 inch) Retina HD Display",
+        "camera": "12MP Rear Camera, 7MP Front Camera",
+        "description": "Widescreen HD LCD Retina Multi-touch IPS, Display (1400:1 Contrast Ratio (Typical), True Tone (P3), Haptic Touch, 625 nits Max Brightness (Typical), Fingerprint-resistant Oleophobic Coating, Display Zoom",
+        "productImage": "http://atlas-content-cdn.pixelsquid.com/stock-images/iphone-x-smartphone-xwVXQLD-600.jpg"
+    },
+    {
+        "id": 7,
+        "name": "APPLE iPhone 13 Mini",
+        "color": "Pink",
+        "ROM": 128,
+        "price": 1100,
+        "modelName": "iPhone 13 Mini",
+        "modelNumber": "MLK13HN/A",
+        "size": "13.7 cm (5.4 inch) Super Retina XDR Display",
+        "camera": "12MP Dual Rear Camera, 12MP Front Camera",
+        "description": "Super Retina XDR display, Haptic Touch, 800 nits Max Brightness (Typical), Fingerprint-resistant Oleophobic Coating, Support for Display of Multiple Languages and Characters",
+        "productImage": "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-mini-pink-select-2021"
+    }
+]
+
+
---
+++ index.js
@@ -0,0 +1,61 @@
+
+// const readline = require("readline");
+
+
+// const rl = readline.createInterface({
+//     input:process.stdin,
+//     output:process.stdout
+// })
+// rl.question("enter your name:",(data)=>{
+//   console.log("your name is:",data);
+//   rl.close()
+// })
+// rl.on("close",()=>{
+//     console.log("programe clsed");
+//     process.exit(0)
+// })
+
+// const fs = require("fs");
+// const data = fs.readFileSync("./files/input.txt","utf-8")
+// fs.writeFileSync("./files/output.txt","this is writing method testing")
+// console.log(data);
+
+// fs.readFile(`./files/input.txt`, "utf-8", (erroer1, data1) => {
+//     console.log(data1);
+//     fs.readFile(`./files/${data1}.txt`, "utf-8", (erroer2, data2) => {
+//         console.log(data2);
+//         fs.writeFile(`./files/latestfile.txt`,`${data1}_date:${Date.now()}_${data2}`,(erroer3,data3)=>{
+//            console.log(data3);
+//         })
+//     })
+// })
+
+const http = require("http")
+const fs = require("fs");
+const html = fs.readFileSync("./files/index.html", "utf-8")
+const app = http.createServer((req, res) => {
+    const path = req.url
+    if (path == "/" || path.toLowerCase() == "/home") {
+        res.writeHead(200,{
+            "Content-type":"text/html",
+            "my-header":"this is my on website"
+        })
+        res.end(html.replace("{{%CONTENT%}}", "you are in Home page"))
+    } else if (path.toLowerCase() == "/contact") {
+        res.writeHead(200)
+        res.end(html.replace("{{%CONTENT%}}", "you are in Contact page"))
+    } else if (path.toLowerCase() == "/about") {
+        res.writeHead(200)
+        res.end(html.replace("{{%CONTENT%}}", "you are in About page"))
+    } else if (path.toLowerCase() == "/products") {
+        res.writeHead(200)
+        res.end(html.replace("{{%CONTENT%}}", "you are in Product page"))
+        fs.readFile("")
+    } else {
+        res.writeHead(404)
+        res.end(html.replace("{{%CONTENT%}}", "404:page note found"))
+    }
+})
+app.listen(8000, '127.0.0.1', () => {
+    console.log("server start");
+})
